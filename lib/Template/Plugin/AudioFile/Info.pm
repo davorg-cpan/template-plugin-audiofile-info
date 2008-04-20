8fedab2e9e83fc54c376e207c1c414bc4ec2f56d
Dave Cross <dave@dave.org.uk>
2008-04-20 16:57:08 +0000

    Switch to svn revisions in $VERSION

--- lib/Template/Plugin/AudioFile/Info.pm
+++ lib/Template/Plugin/AudioFile/Info.pm
@@ -11,7 +11,7 @@ require Exporter;
 
 our @ISA = qw(Exporter AudioFile::Info Template::Plugin);
 
-our $VERSION = sprintf "%d.%02d", '$Revision$ ' =~ /(\d+)\.(\d+)/;
+our $VERSION = sprintf "%d", '$Revision$ ' =~ /(\d+)/;
 
 sub new {
   my ($class, $context, $file) = @_;
