c812446630111315483c42f5fd74d56049702dd6
Dave Cross <dave@dave.org.uk>
2016-10-03 11:22:53 +0100

    We don't use subversion any more.

--- lib/Template/Plugin/AudioFile/Info.pm
+++ lib/Template/Plugin/AudioFile/Info.pm
@@ -11,7 +11,7 @@ require Exporter;
 
 our @ISA = qw(Exporter AudioFile::Info Template::Plugin);
 
-our $VERSION = sprintf "%d", '$Revision$ ' =~ /(\d+)/;
+our $VERSION = '1.5.0';
 
 sub new {
   my ($class, $context, $file) = @_;
